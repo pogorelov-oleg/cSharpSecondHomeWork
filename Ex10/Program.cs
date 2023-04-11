e6bb2262f8fe94ded64ef8f37587c2ffd8f98dd6
pogorelov <89607888555@mail.ru>
2023-04-12 02:08:49 +0700

    Ex10 done

---
+++ Ex10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+int a1 = userNumber / 100;
+int a2 = userNumber % 10;
+int result = (userNumber - a1 * 100 - a2) / 10;
+Console.WriteLine($"{userNumber} -> {result}");
