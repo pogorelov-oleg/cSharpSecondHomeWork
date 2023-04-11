8a2c322524410bc605815476d5cb906ea0275d47
pogorelov <89607888555@mail.ru>
2023-04-12 02:52:35 +0700

    Ex10 edit.04

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -7,7 +7,7 @@
     {
         int a1 = userNumber / 100;
         int a2 = userNumber % 10;
-        //  int result = (userNumber - a1 * 100 - a2) / 10;                      первая версия вычислений...слишком душная и сложная, версия из "Этолонного решения" проще.
+        //  int result = (userNumber - a1 * 100 - a2) / 10;                      первая версия вычислений...слишком душная и сложная, версия из "Эталонного решения" проще.
         int result = userNumber / 10 % 10;
         Console.WriteLine($"Число: {userNumber}. Вторая цифра: {result}.");
         break;
