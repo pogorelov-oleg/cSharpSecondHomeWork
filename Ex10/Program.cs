f4ca7dfefddb3e0b5ce3e2bb3e67ebf80c86728d
pogorelov <89607888555@mail.ru>
2023-04-12 23:34:03 +0700

    Ex10 edit.05

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -1,15 +1,26 @@
 ﻿while (true)
 {
-    Console.Write("Введите трехзначное число: ");
-    string userInput = Console.ReadLine() ?? "";
-    int userNumber = int.Parse(userInput);
-    if (userNumber >= 100 && userNumber < 1000)
+    int userNumber(string message)
     {
-        int a1 = userNumber / 100;
-        int a2 = userNumber % 10;
+        while (true)
+        {
+            Console.Write(message);
+            int userInput;
+            if (int.TryParse(Console.ReadLine(), out userInput))
+            {
+                return userInput;
+            }
+            else Console.WriteLine("Вы ввели некорректные данные ");
+        }
+    }
+    int number = userNumber("Введите трехзначное число: ");
+    if (number >= 100 && number < 1000)
+    {
+        int a1 = number / 100;
+        int a2 = number % 10;
         //  int result = (userNumber - a1 * 100 - a2) / 10;                      первая версия вычислений...слишком душная и сложная, версия из "Эталонного решения" проще.
-        int result = userNumber / 10 % 10;
-        Console.WriteLine($"Число: {userNumber}. Вторая цифра: {result}.");
+        int result = number / 10 % 10;
+        Console.WriteLine($"Число: {number}. Вторая цифра: {result}.");
         break;
     }
     else Console.WriteLine("Вы ввели неверное число.");
