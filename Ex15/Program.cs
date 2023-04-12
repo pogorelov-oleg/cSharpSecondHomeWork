129faebafe85dda40837d652a074c0c420edef9d
pogorelov <89607888555@mail.ru>
2023-04-12 22:00:22 +0700

    Ex15 done

---
+++ Ex15/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите номер дня недели: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+if (userNumber == 1)
+{
+    Console.Write("Понедельник");
+}
+else if (userNumber == 2)
+{
+    Console.Write("Вторник");
+}
+else if (userNumber == 3)
+{
+    Console.Write("Среда");
+}
+else if (userNumber == 4)
+{
+    Console.Write("Четверг");
+}
+else if (userNumber == 5)
+{
+    Console.Write("Пятница");
+}
+else if (userNumber == 6)
+{
+    Console.Write("Суббота");
+}
+else if (userNumber == 7)
+{
+    Console.Write("Воскресенье");
+}
+else Console.Write("Нет такого дня недели");
