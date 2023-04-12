aee17bcbc40263f06cfba112a1d37e195e309adf
pogorelov <89607888555@mail.ru>
2023-04-12 23:19:07 +0700

    Ex13 edit.04

--- Ex13/Program.cs
+++ Ex13/Program.cs
@@ -1,13 +1,24 @@
-﻿Console.Write("Введите целое число: ");
-string userInput = Console.ReadLine() ?? "";
-int userNumber = int.Parse(userInput);
-if (userNumber > 100)
+﻿int userNumber(string message)
 {
-    while (userNumber > 1000)
+    while (true)
     {
-        userNumber /= 10;
+        Console.Write(message);
+        int userInput;
+        if (int.TryParse(Console.ReadLine(), out userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine("Вы ввели некорректные данные ");
     }
-    int thirdNumber = userNumber % 10;
-     Console.Write($"В числе {userNumber} третья цифра: {thirdNumber}");
 }
-else Console.Write($"В числе {userNumber} нет третьей цифры");
+int number = userNumber("Введите целое число: ");
+if (number > 100)
+{
+    while (number > 1000)
+    {
+        number /= 10;
+    }
+    int thirdNumber = number % 10;
+    Console.Write($"В числе {number} третья цифра: {thirdNumber}");
+}
+else Console.Write($"В числе {number} нет третьей цифры");
